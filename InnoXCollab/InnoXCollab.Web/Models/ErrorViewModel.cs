08468051b8124623cf393f012712a69f93835f2f
junarik <113181044+junarik@users.noreply.github.com>
2023-09-15 18:12:54 +0300

    Creating new InnoXCollab
    I've added builder.Service.AddRazzor Pages for RunTime Compilation

---
+++ InnoXCollab/InnoXCollab.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InnoXCollab.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
