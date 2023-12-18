4a77609d55b29862423bbf763df0e0c18f4879bb
junarik <113181044+junarik@users.noreply.github.com>
2023-12-18 06:20:43 -0800

    HOME PAGE MIN STYLE
    ДОРОБКА

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,24 +1,54 @@
-import { Box, Button, Container, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import {Box, Button, Typography} from "@mui/material";
 import { observer } from "mobx-react-lite";
+import {Link} from "react-router-dom";
 
 export default observer(function HomePage() {
     return (
-        <Box className="masthead">
-            <Container sx={{ display: "flex", alignItems: "center", justifyContent: "center", color: "white" }}>
-                <Box>
-                    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", cursor: "default" }}>
-                        <Typography variant="h2">InnoXCollab</Typography>
-                    </Box>
-                    <Box textAlign="center" my="10px" >
-                        <Typography variant="h5">Welcome to InnoXCollab</Typography>
-                        <Button component={Link} to="/events"
-                            variant="outlined" sx={{ color: "White", borderColor: "white", px: "20px", py: "10px", my: 1 }}>
-                            Go to events
+        <Box
+            className="containerH"
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh", // This ensures the container takes up the full viewport height
+            }}
+        >
+            <Box sx={{ textAlign: "center" }}>
+                <Box className="colH">
+                    <Typography variant="h1" className="h1H">
+                        InnoXCollab
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        className="pH"
+                        sx={{
+                            marginBottom: 2,
+                            textAlign: "center",
+                            fontSize: "24px",
+                        }}
+                    >
+                        Every exciting event is a step towards your success. Unleash your
+                        potential with our informational portal!
+                    </Typography>
+                    <Box>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className="buttonH"
+                            type="button"
+                            sx={{
+                                borderRadius: "20px",
+                                marginTop: 1,
+                                backgroundColor: "white",
+                                color: "black",
+                            }}
+                            component={Link} to="/events"
+                        >
+                            Explore Events
                         </Button>
                     </Box>
                 </Box>
-            </Container>
-        </Box >
-    )
-})
+            </Box>
+        </Box>
+    );
+});
